dee8bcceb75b9edb6dc5c450fccaa327ed44cef5
ugwum <nnadiug@rocketmail.com>
2017-11-23 11:41:44 +0100

    added opencv library

--- app/src/main/java/com/generalsteinacoz/gardendashboard/MainActivity.java
+++ app/src/main/java/com/generalsteinacoz/gardendashboard/MainActivity.java
@@ -3,15 +3,14 @@ package com.generalsteinacoz.gardendashboard;
 
 
 import android.app.Activity;
-import android.app.IntentService;
-import android.content.Intent;
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.*;
-import com.gardendashboard.services.BackgroundServices;
-//import com.gardendashboard.threads.PubConSubThread;
-//import com.gardendashboard.threads.PubPublishThread;
 import com.helpers.PubnubConfig;
 import com.pubnub.api.PubNub;
 import com.pubnub.api.callbacks.PNCallback;
@@ -21,17 +20,30 @@ import com.pubnub.api.models.consumer.PNPublishResult;
 import com.pubnub.api.models.consumer.PNStatus;
 import com.pubnub.api.models.consumer.pubsub.PNMessageResult;
 import com.pubnub.api.models.consumer.pubsub.PNPresenceEventResult;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.*;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     PubnubConfig pubnubConfig;
     PubNub pubnub;
 
     //create button widgets
     Button btn_up, btn_down, btn_right, btn_left, connect_btn;
+
+    //create Switch Widgets
     Switch btn_arm, btn_water, btn_torch;
 
     //create textview widgets
@@ -46,13 +58,111 @@ public class MainActivity extends Activity {
     private final String humdity = "humdity";
     private final String soil_moisture = "soil_moisture";
 
-    PubConSubThread pubConSubThread;// = new PubConSubThread();
-    PubPublishThread pubPublishThread;// = new PubPublishThread();
-    BackgroundServices backgroundServices;
+    PubConSubThread pubConSubThread;// new PubConSubThread();
+    PubPublishThread pubPublishThread;// new PubPublishThread();
 
-
+    // message and channel that will be used during publishing
     String payload;
     String channel;
+
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private boolean mIsJavaCamera = true;
+
+
+    private static final String    TAG                 = "OCVSample::Activity";
+    private static final Scalar FACE_RECT_COLOR     = new Scalar(0, 255, 0, 255);
+    public static final int        JAVA_DETECTOR       = 0;
+    public static final int        NATIVE_DETECTOR     = 1;
+
+    private MenuItem               mItemFace50;
+    private MenuItem               mItemFace40;
+    private MenuItem               mItemFace30;
+    private MenuItem               mItemFace20;
+    private MenuItem mItemType;
+
+    private Mat                    mRgba;
+    private Mat                    mGray;
+    private File mCascadeFile;
+    private CascadeClassifier mJavaDetector;
+    private DetectionBasedTracker  mNativeDetector;
+
+    private int                    mDetectorType       = JAVA_DETECTOR;
+    private String[]               mDetectorName;
+
+    private float                  mRelativeFaceSize   = 0.2f;
+    private int                    mAbsoluteFaceSize   = 0;
+
+
+
+
+
+
+
+    private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+
+                    // Load native library after(!) OpenCV initialization
+                    System.loadLibrary("detection_based_tracker");
+
+                    try {
+                        // load cascade file from application resources
+                        InputStream is = getResources().openRawResource(R.raw.lbpcascade_frontalface);
+                        File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+                        mCascadeFile = new File(cascadeDir, "lbpcascade_frontalface.xml");
+                        FileOutputStream os = new FileOutputStream(mCascadeFile);
+
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        while ((bytesRead = is.read(buffer)) != -1) {
+                            os.write(buffer, 0, bytesRead);
+                        }
+                        is.close();
+                        os.close();
+
+                        mJavaDetector = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+                        if (mJavaDetector.empty()) {
+                            Log.e(TAG, "Failed to load cascade classifier");
+                            mJavaDetector = null;
+                        } else
+                            Log.i(TAG, "Loaded cascade classifier from " + mCascadeFile.getAbsolutePath());
+
+                        mNativeDetector = new DetectionBasedTracker(mCascadeFile.getAbsolutePath(), 0);
+
+                        cascadeDir.delete();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Log.e(TAG, "Failed to load cascade. Exception thrown: " + e);
+                    }
+
+                    mOpenCvCameraView.enableView();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
+
+
+
+    public MainActivity() {
+        mDetectorName = new String[2];
+        mDetectorName[JAVA_DETECTOR] = "Java";
+        mDetectorName[NATIVE_DETECTOR] = "Native (tracking)";
+
+        Log.i(TAG, "Instantiated new " + this.getClass());
+    }
+
+
 
 
 
@@ -61,11 +171,19 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if(!OpenCVLoader.initDebug()){
+            Log.d(TAG, "OpenCV not loaded");
+            Toast.makeText(this, "OpenCV not loaded", Toast.LENGTH_LONG).show();
+        } else {
+            Log.d(TAG, "OpenCV loaded");
+            Toast.makeText(this, "OpenCV loaded", Toast.LENGTH_LONG).show();
+        }
+
         //initialize Pubnub Configuration and PubNub
         pubnubConfig = new PubnubConfig();
         pubnub = new PubNub(pubnubConfig.pConfig());
 
-        //connecting buttons
+        //getting the buttons
         btn_up = (Button) findViewById(R.id.btn_up);
         btn_down = (Button) findViewById(R.id.btn_down);
         btn_right = (Button) findViewById(R.id.btn_right);
@@ -76,7 +194,7 @@ public class MainActivity extends Activity {
         btn_water = (Switch) findViewById(R.id.switch_water);
         btn_torch = (Switch) findViewById(R.id.switch_torch);
 
-        //connecting textviews
+        //getting textviews
         activity_txtView = (TextView) findViewById(R.id.activity_txtView);
         status_txtView = (TextView) findViewById(R.id.status_txtView);
         connect_txtView = (TextView) findViewById(R.id.connect_txtView);
@@ -149,6 +267,11 @@ public class MainActivity extends Activity {
 
 
 
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.java_surface_view);
+
+        mOpenCvCameraView.setVisibility(CameraBridgeViewBase.VISIBLE);
+
+        mOpenCvCameraView.setCvCameraViewListener(this);
 
 
 
@@ -156,15 +279,13 @@ public class MainActivity extends Activity {
 
 
     void pubnubSubscribe(){
-        //backgroundServices = new BackgroundServices();
+
         pubPublishThread = new PubPublishThread();
         pubConSubThread = new PubConSubThread();
-        //connect_btn.setTextColor(Color.BLUE);
-        //connect_btn.setText("Connecting");
+
 
         try {
-            //Intent is = new Intent(MainActivity.this, BackgroundServices.class);
-            //backgroundServices.startService(is);
+
             pubConSubThread.start();
             pubPublishThread.start();
         }catch (Exception e){
@@ -172,30 +293,36 @@ public class MainActivity extends Activity {
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
         }
 
-        //Intent serviceIntent = new Intent(MainActivity.this, BackgroundServices.class);
-        //backgroundServices.startService(serviceIntent);
+
 
     }
 
-  public void updateConnectBtn(int colour, String connectStatus){
+  /**public void updateConnectBtn(int colour, String connectStatus){
       connect_btn.setTextColor(colour);
       connect_btn.setText(connectStatus);
-  }
+  }**/
 
+  //this method is called from the PubConSub thread
   public void updateActivityTxtView(String message){
       activity_txtView.setText(message);
   }
 
+    /**this method is called from the Pubpublish thread
+        and updates the status textView **/
     public void updatePubTextView(int colour, String publishStatus){
         status_txtView.setTextColor(colour);
         status_txtView.setText(publishStatus);
     }
 
+    /**this method is called from the PubConSub thread
+     and updates the connect textView **/
     public void updateConnectTextView(int colour, String connectStatus){
         connect_txtView.setTextColor(colour);
         connect_txtView.setText(connectStatus);
     }
 
+    /**this method is called from the Pubpublish thread
+     and updates the amb. temp textView **/
     public void updateAmbTempTextView(String value){
         if (Integer.parseInt(value) > 35){
             a_temp_txtView.setTextColor(Color.RED);
@@ -207,6 +334,8 @@ public class MainActivity extends Activity {
 
     }
 
+    /**this method is called from the Pubpublish thread
+     and updates the soil temp textView **/
     public void updateSoilTempTextView(String value){
         if (Integer.parseInt(value) < 35){
             s_temp_txtView.setTextColor(Color.RED);
@@ -217,6 +346,8 @@ public class MainActivity extends Activity {
         }
     }
 
+    /**this method is called from the Pubpublish thread
+     and updates the status textView **/
     public void updateHumdityTextView(String value){
         if (Integer.parseInt(value) < 70){
             humdity_txtView.setTextColor(Color.RED);
@@ -317,7 +448,107 @@ public class MainActivity extends Activity {
     }
 
 
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
 
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+
+    @Override
+    public void onCameraViewStarted(int i, int i1) {
+        mGray = new Mat();
+        mRgba = new Mat();
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mGray.release();
+        mRgba.release();
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame cvCameraViewFrame) {
+
+
+        mRgba = cvCameraViewFrame.rgba();
+        mGray = cvCameraViewFrame.gray();
+
+        if (mAbsoluteFaceSize == 0) {
+            int height = mGray.rows();
+            if (Math.round(height * mRelativeFaceSize) > 0) {
+                mAbsoluteFaceSize = Math.round(height * mRelativeFaceSize);
+            }
+            mNativeDetector.setMinFaceSize(mAbsoluteFaceSize);
+        }
+
+        MatOfRect faces = new MatOfRect();
+
+        if (mDetectorType == JAVA_DETECTOR) {
+            if (mJavaDetector != null)
+                mJavaDetector.detectMultiScale(mGray, faces, 1.1, 2, 2, // TODO: objdetect.CV_HAAR_SCALE_IMAGE
+                        new Size(mAbsoluteFaceSize, mAbsoluteFaceSize), new Size());
+        }
+        else if (mDetectorType == NATIVE_DETECTOR) {
+            if (mNativeDetector != null)
+                mNativeDetector.detect(mGray, faces);
+        }
+        else {
+            Log.e(TAG, "Detection method is not selected!");
+        }
+
+        Rect[] facesArray = faces.toArray();
+        for (int i = 0; i < facesArray.length; i++)
+            Imgproc.rectangle(mRgba, facesArray[i].tl(), facesArray[i].br(), FACE_RECT_COLOR, 3);
+
+        return mRgba;
+
+    }
+
+
+
+
+    private void setMinFaceSize(float faceSize) {
+        mRelativeFaceSize = faceSize;
+        mAbsoluteFaceSize = 0;
+    }
+
+    private void setDetectorType(int type) {
+        if (mDetectorType != type) {
+            mDetectorType = type;
+
+            if (type == NATIVE_DETECTOR) {
+                Log.i(TAG, "Detection Based Tracker enabled");
+                mNativeDetector.start();
+            } else {
+                Log.i(TAG, "Cascade detector enabled");
+                mNativeDetector.stop();
+            }
+        }
+    }
 
 
 
@@ -467,10 +698,8 @@ public class MainActivity extends Activity {
                     public void onResponse(PNPublishResult result, PNStatus status) {
                         if (!status.isError()) {
                             publishProgress(Color.GREEN, "publish success");
-
                         } else {
                             publishProgress(Color.RED, status.getCategory().toString());
-
                             status.retry();
                         }
 
